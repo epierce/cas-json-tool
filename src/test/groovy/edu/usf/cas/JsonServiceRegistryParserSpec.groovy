3ea7d2f210379d10ff9256b8ece2a07356e5143b
Eric Pierce <epierce@usf.edu>
2012-10-31 20:54:32 -0400

    Added Spock Tests
    Spock tests for all JsonServiceRegistryTool functions

--- src/test/groovy/edu/usf/cas/JsonServiceRegistryParserSpec.groovy
+++ src/test/groovy/edu/usf/cas/JsonServiceRegistryParserSpec.groovy
@@ -2,7 +2,6 @@ package edu.usf.cas
 
 import spock.lang.*
 
-
 class JsonServiceRegistryParserSpec extends spock.lang.Specification {
 
     def "Empty JSON Service Registry"() {
@@ -11,39 +10,37 @@ class JsonServiceRegistryParserSpec extends spock.lang.Specification {
         def jsonParser = new JsonServiceRegistryParser()
 
         then:
-        jsonParser.jsonObject == [ services:[] ]
-    }
-
-    def "JSON input file doesn't exist"() {
-    	given:
-    	def jsonParser = new JsonServiceRegistryParser()
-
-        when:
-        jsonParser.readInputFile("file_does_not_exist.json")
-
-        then:
-   		thrown(java.io.FileNotFoundException)
-    }
-
-    def "input file with bad JSON data"() {
-        given:
-        def jsonParser = new JsonServiceRegistryParser()
-
-        when:
-        jsonParser.readInputFile("src/test/resources/bad_format.json")
-
-        then:
-        thrown(groovy.json.JsonException)
+        jsonParser.jsonData == [ services:[] ]
     }
     
     def "input file without valid JSON ServiceRegistry"() {
         given:
+        def jsonTool = new JsonServiceRegistryTool()
+        def object = jsonTool.readInputFile("src/test/resources/bad_serviceRegistry.json")
         def jsonParser = new JsonServiceRegistryParser()
 
         when:
-        jsonParser.readInputFile("src/test/resources/bad_serviceRegistry.json")
+        jsonParser.setJsonData(object)
 
         then:
         thrown(edu.usf.cas.JsonServiceRegistryFileFormatException)
     }
+
+    def "input file with valid JSON ServiceRegistry"() {
+        given:
+        def jsonTool = new JsonServiceRegistryTool()
+        def object = jsonTool.readInputFile("src/test/resources/serviceRegistry.json")
+        def jsonParser = new JsonServiceRegistryParser()
+
+        when:
+        jsonParser.setJsonData(object)
+
+        then:
+        jsonParser.jsonData.services[0].ssoEnabled == true
+        jsonParser.jsonData.services[0].name == "Example Application"
+        jsonParser.jsonData.services[0].description == "This is an example application"
+        jsonParser.jsonData.services[0].ssoEnabled == true
+    }    
+
+
 }
