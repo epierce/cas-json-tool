aecb95880f359069e5ce073f3d09f5dca45bd28d
Eric Pierce <epierce@usf.edu>
2013-09-22 16:11:09 -0400

    Tests for Role-Based Authorization options

--- src/test/groovy/edu/usf/cims/cas/jsontool/JsonServiceRegistryToolSpec.groovy
+++ src/test/groovy/edu/usf/cims/cas/jsontool/JsonServiceRegistryToolSpec.groovy
@@ -132,67 +132,67 @@ class JsonServiceRegistryToolSpec extends spock.lang.Specification {
     }
 
     def "PostProcessor success"() {
-        given:
-        def jsonTool = new JsonServiceRegistryTool()
-        String[] args = ['--input=src/test/resources/serviceRegistry.json','--output=PostProcessor.test.json']
-        def opt = jsonTool.getCommandLineOptions(args)
-        def config = jsonTool.getConfigSettings(opt)
-        config.postCommand = "true"
-        def jsonParser = jsonTool.createJSONparser(config,opt)
-        def result = jsonTool.runAction(jsonParser,opt)
-        jsonTool.printJSON(result)
+      given:
+      def jsonTool = new JsonServiceRegistryTool()
+      String[] args = ['--input=src/test/resources/serviceRegistry.json','--output=PostProcessor.test.json']
+      def opt = jsonTool.getCommandLineOptions(args)
+      def config = jsonTool.getConfigSettings(opt)
+      config.postCommand = "true"
+      def jsonParser = jsonTool.createJSONparser(config,opt)
+      def result = jsonTool.runAction(jsonParser,opt)
+      jsonTool.printJSON(result)
 
-        when:
-        jsonTool.runPostProcessor(config,opt)
+      when:
+      jsonTool.runPostProcessor(config,opt)
 
-        then:
-        notThrown(groovy.util.ScriptException)
+      then:
+      notThrown(groovy.util.ScriptException)
 
-        cleanup:
-        def file = new File("PostProcessor.test.json")
-        file.delete()
+      cleanup:
+      def file = new File("PostProcessor.test.json")
+      file.delete()
     }
 
-       def "Force option required to overwrite output JSON file"() {
-        given:
-        def file = new File("write_fail.test.json")
-        file.createNewFile()
+     def "Force option required to overwrite output JSON file"() {
+      given:
+      def file = new File("write_fail.test.json")
+      file.createNewFile()
 
-        def jsonTool = new JsonServiceRegistryTool()
-        String[] args = ['--input=src/test/resources/serviceRegistry.json','--output=write_fail.test.json']
-        def opt = jsonTool.getCommandLineOptions(args)
-        def config = jsonTool.getConfigSettings(opt)
+      def jsonTool = new JsonServiceRegistryTool()
+      String[] args = ['--input=src/test/resources/serviceRegistry.json','--output=write_fail.test.json']
+      def opt = jsonTool.getCommandLineOptions(args)
+      def config = jsonTool.getConfigSettings(opt)
 
-        when:
-        def jsonParser = jsonTool.createJSONparser(config,opt)
+      when:
+      def jsonParser = jsonTool.createJSONparser(config,opt)
 
-        then:
-        def e = thrown(java.io.FileNotFoundException)
-        e.message == "write_fail.test.json already exists.  Use --force to overwrite."
+      then:
+      def e = thrown(java.io.FileNotFoundException)
+      e.message == "write_fail.test.json already exists.  Use --force to overwrite."
 
-        cleanup:
-        file.delete()
+      cleanup:
+      file.delete()
     }
 
-      def "Force option required to overwrite output CSV file"() {
-	given:
-	def file = new File("write_fail.test.csv")
-	file.createNewFile()
+    def "Force option required to overwrite output CSV file"() {
+      given:
+      def file = new File("write_fail.test.csv")
+      file.createNewFile()
 
-	def jsonTool = new JsonServiceRegistryTool()
-	String[] args = ['--input=src/test/resources/serviceRegistry.json','--csv=write_fail.test.csv']
-	def opt = jsonTool.getCommandLineOptions(args)
-	def config = jsonTool.getConfigSettings(opt)
+      def jsonTool = new JsonServiceRegistryTool()
+      String[] args = ['--input=src/test/resources/serviceRegistry.json','--csv=write_fail.test.csv']
+      def opt = jsonTool.getCommandLineOptions(args)
+      def config = jsonTool.getConfigSettings(opt)
 
-	when:
-	def jsonParser = jsonTool.createJSONparser(config,opt)
+      when:
+      def jsonParser = jsonTool.createJSONparser(config,opt)
 
-	then:
-	def e = thrown(java.io.FileNotFoundException)
-	e.message == "write_fail.test.csv already exists.  Use --force to overwrite."
+      then:
+      def e = thrown(java.io.FileNotFoundException)
+      e.message == "write_fail.test.csv already exists.  Use --force to overwrite."
 
-	cleanup:
-	file.delete()
+      cleanup:
+      file.delete()
     }
 
     def "Cannot write to output JSON file"() {
@@ -218,25 +218,25 @@ class JsonServiceRegistryToolSpec extends spock.lang.Specification {
     }
 
     def "Cannot write to output CSV file"() {
-	given:
-	def file = new File("write_fail.test.csv")
-	file.createNewFile()
-	file.setWritable(false)
+      given:
+      def file = new File("write_fail.test.csv")
+      file.createNewFile()
+      file.setWritable(false)
 
-	def jsonTool = new JsonServiceRegistryTool()
-	String[] args = ['--input=src/test/resources/serviceRegistry.json','--csv=write_fail.test.csv','--force']
-	def opt = jsonTool.getCommandLineOptions(args)
-	def config = jsonTool.getConfigSettings(opt)
+      def jsonTool = new JsonServiceRegistryTool()
+      String[] args = ['--input=src/test/resources/serviceRegistry.json','--csv=write_fail.test.csv','--force']
+      def opt = jsonTool.getCommandLineOptions(args)
+      def config = jsonTool.getConfigSettings(opt)
 
-	when:
-	def jsonParser = jsonTool.createJSONparser(config,opt)
+      when:
+      def jsonParser = jsonTool.createJSONparser(config,opt)
 
-	then:
-	thrown(java.io.FileNotFoundException)
+      then:
+      thrown(java.io.FileNotFoundException)
 
-	cleanup:
-	file.setWritable(true)
-	file.delete()
+      cleanup:
+      file.setWritable(true)
+      file.delete()
     }
 
     def "--name is required"() {
@@ -370,10 +370,6 @@ class JsonServiceRegistryToolSpec extends spock.lang.Specification {
         def e = thrown(java.lang.IllegalArgumentException)
         e.message == "--disableProxy and --enableProxy cannot be used together"
     }
-
-
-
-
 
     def "URL does not match the ServiceId Ant Pattern"() {
         given:
@@ -614,6 +610,74 @@ class JsonServiceRegistryToolSpec extends spock.lang.Specification {
 
     }
 
+    def "Create new service with role-based access controls"() {
+        given:
+        def jsonTool = new JsonServiceRegistryTool()
+        String[] args = [   '--new',
+                            '--name=My Service', 
+                            '--desc=Test Service', 
+                            '--pattern=https://example.org/**', 
+                            '--url=https://example.org/index.php',
+                            '--authzName=memberOf',
+                            '--authzValue=group1,group2'
+                        ]
+        def opt = jsonTool.getCommandLineOptions(args)
+        def config = jsonTool.getConfigSettings(opt)
+        def jsonParser = jsonTool.createJSONparser(config,opt)
+
+        when:
+        def result = jsonTool.runAction(jsonParser,opt)
+
+        then:
+        notThrown(java.lang.IllegalArgumentException)
+        result.services[0].name == "My Service"
+        result.services[0].serviceId == "https://example.org/**"
+        result.services[0].extraAttributes.authzAttributes.memberOf == ["group1","group2"]
+    }
+
+    def "Require authzValue when authzName is given"() {
+        given:
+        def jsonTool = new JsonServiceRegistryTool()
+        String[] args = [   '--new',
+                            '--name=My Service', 
+                            '--desc=Test Service', 
+                            '--pattern=https://example.org/**', 
+                            '--url=https://example.org/index.php',
+                            '--authzName=memberOf'
+                        ]
+        def opt = jsonTool.getCommandLineOptions(args)
+        def config = jsonTool.getConfigSettings(opt)
+        def jsonParser = jsonTool.createJSONparser(config,opt)
+
+        when:
+        def result = jsonTool.runAction(jsonParser,opt)
+
+        then:
+        def e = thrown(java.lang.IllegalArgumentException)
+        e.message == "Authorization values are required when passing a authorization attribute name"
+    }
+
+    def "Require authzValue when authzName is given"() {
+        given:
+        def jsonTool = new JsonServiceRegistryTool()
+        String[] args = [   '--new',
+                            '--name=My Service', 
+                            '--desc=Test Service', 
+                            '--pattern=https://example.org/**', 
+                            '--url=https://example.org/index.php',
+                            '--authzValue=group1,group2'
+                        ]
+        def opt = jsonTool.getCommandLineOptions(args)
+        def config = jsonTool.getConfigSettings(opt)
+        def jsonParser = jsonTool.createJSONparser(config,opt)
+
+        when:
+        def result = jsonTool.runAction(jsonParser,opt)
+
+        then:
+        def e = thrown(java.lang.IllegalArgumentException)
+        e.message == "Authorization attribute name required when passing authorization values"
+    }
     def "Create new service with multiple extra attributes"() {
         given:
         def jsonTool = new JsonServiceRegistryTool()
@@ -702,10 +766,6 @@ class JsonServiceRegistryToolSpec extends spock.lang.Specification {
         result.services[0].evaluationOrder == 50
 
     }
-
-/**
-* Tests for commands that modify existing services
-**/
 
     def "Service ID number required for modify"() {
         given:
@@ -852,14 +912,12 @@ class JsonServiceRegistryToolSpec extends spock.lang.Specification {
         String[] args = [   '--modify',
                             '--id=1',
                             '--input=src/test/resources/serviceRegistry.json',
-                            '--extraAttribute=contactEmail=bruce@wayne-ent.com'
+                            '--extraAttribute=contactEmail=REMOVE'
                         ]
         def opt = jsonTool.getCommandLineOptions(args)
         def config = jsonTool.getConfigSettings(opt)
         config.allowedExtraAttributes = ['contactEmail','contactName']
-        config.requiredExtraAttributes = ['contactEmail', 'contactName']
         def jsonParser = jsonTool.createJSONparser(config,opt)
-        assert jsonParser.jsonData.services[0].extraAttributes.contactEmail == ["admin@example.edu"]
 
         when:
         def result = jsonTool.runAction(jsonParser,opt)
@@ -867,7 +925,7 @@ class JsonServiceRegistryToolSpec extends spock.lang.Specification {
         then:
         notThrown(java.lang.IllegalArgumentException)
         result.services.last().extraAttributes.contactName == ["Admin User"]
-        result.services.last().extraAttributes.contactEmail == ["bruce@wayne-ent.com"]
+        result.services.last().extraAttributes.contactEmail == null
     }
 
     def "Do not allow a required extraAttribute to be removed"() {
@@ -893,9 +951,52 @@ class JsonServiceRegistryToolSpec extends spock.lang.Specification {
         e.message == "Attribute contactEmail is required!"
     }
 
-/**
-* Tests for commands that remove services
-**/
+    def "Require authzValue when authzName is given"() {
+        given:
+        def jsonTool = new JsonServiceRegistryTool()
+        String[] args = [   '--modify',
+                            '--id=1',
+                            '--input=src/test/resources/serviceRegistry.json',
+                            '--authzName=memberOf'
+                        ]
+        def opt = jsonTool.getCommandLineOptions(args)
+        def config = jsonTool.getConfigSettings(opt)
+        config.allowedExtraAttributes = ['contactEmail','contactName']
+        config.requiredExtraAttributes = ['contactEmail', 'contactName']
+        def jsonParser = jsonTool.createJSONparser(config,opt)
+        assert jsonParser.jsonData.services[0].extraAttributes.contactEmail == ["admin@example.edu"]
+
+        when:
+        def result = jsonTool.runAction(jsonParser,opt)
+
+        then:
+        def e = thrown(java.lang.IllegalArgumentException)
+        e.message == "Authorization values are required when passing a authorization attribute name"
+    }
+
+    def "Require authzValue when authzName is given"() {
+        given:
+        def jsonTool = new JsonServiceRegistryTool()
+        String[] args = [   '--modify',
+                            '--id=1',
+                            '--input=src/test/resources/serviceRegistry.json',
+                            '--authzValue=group1,group2'
+                        ]
+        def opt = jsonTool.getCommandLineOptions(args)
+        def config = jsonTool.getConfigSettings(opt)
+        config.allowedExtraAttributes = ['contactEmail','contactName']
+        config.requiredExtraAttributes = ['contactEmail', 'contactName']
+        def jsonParser = jsonTool.createJSONparser(config,opt)
+        assert jsonParser.jsonData.services[0].extraAttributes.contactEmail == ["admin@example.edu"]
+
+        when:
+        def result = jsonTool.runAction(jsonParser,opt)
+
+        then:
+        def e = thrown(java.lang.IllegalArgumentException)
+        e.message == "Authorization attribute name required when passing authorization values"
+    }
+
     def "Service ID number required for remove"() {
         given:
         def jsonTool = new JsonServiceRegistryTool()
@@ -937,9 +1038,7 @@ class JsonServiceRegistryToolSpec extends spock.lang.Specification {
         jsonParser.jsonData.services.size() == 1
     }
 
-/**
-* Tests for commands that search for services
-**/
+
     def "Service ID, name or URL required for search"() {
         given:
         def jsonTool = new JsonServiceRegistryTool()
